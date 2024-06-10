6b1088f77dcf3e32c66d9bf8337fd56d6fdf51bd
benj4627 <112342046+benj4627@users.noreply.github.com>
2024-06-10 12:52:33 +0200

    Js færdigt til kontakt form

---
+++ assets/js/index.js
@@ -0,0 +1,19 @@
+//fanger de relevante HTML elementer
+let visibleAnimEl = document.querySelectorAll(".slideinAnim");
+//Sætter en værdi for hvor langt man scroller før function aktiverer. 
+let scrollThreshold = 50;
+
+function toggleClassScroll() {
+  //tilføjer eventListnere som lytter efter scroll-begivenheden. Når brugeren scroller siden, udføres funktionen.
+  window.addEventListener("scroll", function() {
+    //Tjekker, om den aktuelle scroll position (window.scrollY) er større end  det tilladte scrollThreshold på 100
+    if (window.scrollY > scrollThreshold) {
+      //Gennemgår HTML elementer isibleAnimEl og tilføjer klassen animationVisible. 
+      visibleAnimEl.forEach(container => {
+        container.classList.add("animationVisible");
+      });
+    }
+  });
+};
+//kalder function når siden loader, for at tjekke scroll position
+toggleClassScroll();
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,23 @@
-
-//fanger de relevante HTML elementer
-let visibleAnimEl = document.querySelectorAll(".slideinAnim");
-//Sætter en værdi for hvor langt man scroller før function aktiverer. 
-let scrollThreshold = 50;
-
-function toggleClassScroll() {
-  //tilføjer eventListnere som lytter efter scroll-begivenheden. Når brugeren scroller siden, udføres funktionen.
-  window.addEventListener("scroll", function() {
-    //Tjekker, om den aktuelle scroll position (window.scrollY) er større end  det tilladte scrollThreshold på 100
-    if (window.scrollY > scrollThreshold) {
-      //Gennemgår HTML elementer isibleAnimEl og tilføjer klassen animationVisible. 
-      visibleAnimEl.forEach(container => {
-        container.classList.add("animationVisible");
-      });
-    }
-  });
-};
-//kalder function når siden loader, for at tjekke scroll position
-toggleClassScroll();
-
-// hamburger menu js start 
- 
+const chatIconEl = document.querySelector("#contactIcon");
+const contactFormEl = document.querySelector(".contactFormSticky");
 const burgerIcon = document.querySelector(".burgerMenuIcon");
 const notificationsEl = document.querySelector(".subMenu");
+const crossContactEl = document.querySelector("#contactFormClose"); 
 
 
+chatIconEl.addEventListener("click", () => {
+  contactFormEl.classList.toggle("contactFormActive");
+});
+
+crossContactEl.addEventListener("click", () => {
+  contactFormEl.classList.toggle("contactFormActive");
+});
+
+
+// hamburger menu js start 
 burgerIcon.addEventListener("click", () => {
 notificationsEl.classList.toggle("subMenuOpen");
 });
 
+
 // hamburger menu js slut
-
-//chat funktion
-
-const chatIconEl = document.querySelector("#contactIcon");
-const contactFormEl = document.querySelector(".contactFormSticky");
-
-chatIconEl.addEventListener("click", () => {
-    contactFormEl.classList.toggle("contactFormActive");
-});
