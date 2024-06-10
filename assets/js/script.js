f10efe6d1a8da2edd88f6692a6954505826e3aac
benj4627 <112342046+benj4627@users.noreply.github.com>
2024-06-10 13:00:27 +0200

    kontaktform mobil færdig

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,17 @@
+//Fanger de relevante HTML elementer 
 const chatIconEl = document.querySelector("#contactIcon");
 const contactFormEl = document.querySelector(".contactFormSticky");
 const burgerIcon = document.querySelector(".burgerMenuIcon");
 const notificationsEl = document.querySelector(".subMenu");
 const crossContactEl = document.querySelector("#contactFormClose"); 
 
-
+//tilføjer en addeventListener click til vores chat ikon.
+//herefter tildeles contactFormActive class, som ændrer opacity fra 0-1 og derfor er kontaktformen synlig. 
 chatIconEl.addEventListener("click", () => {
   contactFormEl.classList.toggle("contactFormActive");
 });
 
+//Det samme som ovenover, men her tilføjes der addeventlistenere til "krydset" som toggler active classen. 
 crossContactEl.addEventListener("click", () => {
   contactFormEl.classList.toggle("contactFormActive");
 });
@@ -18,6 +21,5 @@ crossContactEl.addEventListener("click", () => {
 burgerIcon.addEventListener("click", () => {
 notificationsEl.classList.toggle("subMenuOpen");
 });
-
 
 // hamburger menu js slut
